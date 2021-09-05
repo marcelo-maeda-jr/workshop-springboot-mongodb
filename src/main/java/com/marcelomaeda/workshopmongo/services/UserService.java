be14ff89c297d6a668df9a13558a6b0813a1378e
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-09-05 12:08:15 -0300

    Conectando mongodb com repository e service

---
+++ src/main/java/com/marcelomaeda/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.marcelomaeda.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcelomaeda.workshopmongo.domain.User;
+import com.marcelomaeda.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
