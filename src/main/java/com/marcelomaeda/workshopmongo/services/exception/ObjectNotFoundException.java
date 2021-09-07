bd7c150d9491ad284dfc38eeab940def2fce91b5
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-09-07 15:33:33 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/marcelomaeda/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.marcelomaeda.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
