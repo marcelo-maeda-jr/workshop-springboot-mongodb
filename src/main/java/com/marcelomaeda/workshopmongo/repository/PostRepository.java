3bb0dc2d2ce1777a6c4edd396d5e64671d7c42c4
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-09-07 18:02:10 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/marcelomaeda/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.marcelomaeda.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.marcelomaeda.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
